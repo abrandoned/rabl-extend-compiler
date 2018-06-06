9f1aa94c7efac7a8a71225af7763ee8569b354b1
Brandon Dewitt <brandonsdewitt@gmail.com>
2018-06-05 23:22:56 -0700

    only load the tasks if we are using rake

--- lib/rabl/extend/compiler.rb
+++ lib/rabl/extend/compiler.rb
@@ -4,11 +4,11 @@ require "rabl/extend/compiler/version"
 module Rabl
   module Extend
     module Compiler
-      #if defined?(Rake)
-      ::Dir[::File.join(::File.dirname(__FILE__), "..", "..", "tasks", "**", "*.rake")].each do |rake_file|
-        load rake_file
+      if defined?(Rake)
+        ::Dir[::File.join(::File.dirname(__FILE__), "..", "..", "tasks", "**", "*.rake")].each do |rake_file|
+          load rake_file
+        end
       end
-      #end
     end
   end
 end
