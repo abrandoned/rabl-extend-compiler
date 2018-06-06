0012e1eb8e8b3495a739b6b1e56637073533654e
Brandon Dewitt <brandonsdewitt@gmail.com>
2018-06-05 21:40:00 -0700

    add rake tasks

--- lib/rabl/extend/compiler.rb
+++ lib/rabl/extend/compiler.rb
@@ -3,7 +3,11 @@ require "rabl/extend/compiler/version"
 module Rabl
   module Extend
     module Compiler
-      # Your code goes here...
+      if defined?(Rake)
+        ::Dir[::File.join(::File.dirname(__FILE__), "..", "..", "..", "..", "tasks", "**", "*.rake")].each do |rake_file|
+          require rake_file
+        end
+      end
     end
   end
 end
